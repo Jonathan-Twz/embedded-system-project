0ef7448819854de62e9c0107d8d70070ef2dbfd5
Jonathan-Twz@raspberrypi <wenzhe.t99@berkeley.edu>
2020-06-22 10:33:51 +0800

    raspberry setup done

---
+++ ard_code/ard_code.ino
@@ -0,0 +1,34 @@
+#include <dht11.h>
+dht11 DHT;
+#define DHT11_PIN 4
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  int chk;
+  chk = DHT.read(DHT11_PIN); // READ DATA
+//  switch (chk){
+//  case DHTLIB_OK: 
+//    Serial.println("OK,\t"); 
+//    break;
+//  case DHTLIB_ERROR_CHECKSUM: 
+//    Serial.println("Checksum error,\t"); 
+//    break;
+//  case DHTLIB_ERROR_TIMEOUT: 
+//    Serial.println("Time out error,\t"); 
+//    break;
+//  default: 
+//    Serial.println("Unknown error,\t"); 
+//    break;
+//  }
+Serial.print("humidity(%):");
+  Serial.println(DHT.humidity,1);
+  Serial.print("tempe(C):");
+  Serial.println(DHT.temperature,1); 
+  Serial.println(" ");
+  delay(1000);
+}
+
+
