5e4ae0963d10128c5bd21d6d38c5695d8a236f5c
Jonathan_Twz <joanthan_tong@foxmail.com>
2020-06-23 23:43:29 -0700

    dashboard updated

--- web/DHT11-run.py
+++ web/DHT11-run.py
@@ -9,16 +9,7 @@ def index():
     aa = 0
     result = fun(aa)
     
-    return render_template('DHT11-web.html',i = str(result[0])+str(u'\u2103'), j= str(result[1])+'%')
-
-# @app.route('/temp.js')
-# def return_files_tut():
-# 	try:
-# 		return send_file('../temp.js')
-# 	except Exception as e:
-# 		return str(e)
-
-# @app.route('/humidity.js')
+    return render_template('DHT11-web.html',i = str(result[0]), j= str(result[1]))
 
 if __name__=='__main__':
     app.run(host='localhost', port=5000, debug=True)
